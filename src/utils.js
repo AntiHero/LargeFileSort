a32e364081f5709cac53db99c84ec659123efab5
Aleksandr Schemelev <prophreaker@gmail.com>
2021-09-17 13:18:59 +0300

    add: cleanFolder util function

--- src/utils.js
+++ src/utils.js
@@ -1,39 +1,55 @@
 const fsPromises = require('fs/promises');
 const fs = require('fs');
+const { resolve } = require('path');
 const constants = require('./constants');
 
 async function getFileSize(path) {
   const size = (await fsPromises.stat(path)).size;
   return size;
-};
+}
 
 function toBytes(size) {
   return size * constants.KB ** 2;
-};
+}
 
 function toMbs(size) {
   return size / constants.KB ** 2;
-};
+}
 
 function removeFileIfExists(path) {
   if (fs.existsSync(path)) {
     fs.unlinkSync(path);
   }
-};
+}
 
 function calculateProgress(currentValue, requiredValue) {
-  const progress = (currentValue * 100 / requiredValue).toFixed(2);
+  const progress = ((currentValue * 100) / requiredValue).toFixed(2);
   return progress;
 }
 
-function printProgress(progress){
+function printProgress(progress) {
   process.stdout.clearLine();
   process.stdout.cursorTo(0);
-  process.stdout.write(`Current process is ${progress > 100 ? 100 : progress}% completed.`);
-};
+  process.stdout.write(
+    `Current process is ${progress > 100 ? 100 : progress}% completed.`
+  );
+}
+
+/**
+ *
+ * @param {string} path - path to folder which should be cleaned (all files will be deleted)
+ */
+function cleanFolder(path) {
+  const files = fs.readdirSync(path);
+
+  files.forEach((file) => {
+    fs.unlinkSync(resolve(path, file));
+  });
+}
 
 module.exports = {
   calculateProgress,
+  cleanFolder,
   getFileSize,
   printProgress,
   removeFileIfExists,
