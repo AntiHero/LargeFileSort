f7b5365b0d15b06e6e9b854993c0b4e1ab3cb46e
Aleksandr Schemelev <prophreaker@gmail.com>
2021-10-31 22:32:00 +0300

    fix: test

--- src/__tests__/createFile.test.js
+++ src/__tests__/createFile.test.js
@@ -2,6 +2,9 @@ const utils = require('../utils');
 const fs = require('fs');
 const createFileWithRandomNumbers = require('../createFile');
 
+jest.mock('fs');
+console.log(process.cwd())
+
 it('should be a function', () => {
   expect(createFileWithRandomNumbers).toBeInstanceOf(Function);
 });
@@ -9,21 +12,21 @@ it('should be a function', () => {
 const pathToTestFile = './files/test.txt';
 
 describe('check functionality', () => {
-  beforeAll(() => {
+  beforeEach(() => {
     utils.removeFileIfExists(pathToTestFile);
   });
 
-  afterEach(() => {
+  afterAll(() => {
     utils.removeFileIfExists(pathToTestFile);
   });
 
-  it('should create a file at specified path', async () => {
+  it.only('should create a file at specified path', async () => {
     await createFileWithRandomNumbers(0.001, pathToTestFile);
     expect(fs.existsSync(pathToTestFile)).toBeTruthy();
   });
 
   it('should create a file of specified size', async () => {
-    const requiredSize = 0.0001;
+    const requiredSize = 0.005;
     await createFileWithRandomNumbers(requiredSize, pathToTestFile);
     const currentSize = await utils.getFileSize(pathToTestFile);
     expect(Number(utils.toMbs(currentSize).toFixed(4))).toBe(requiredSize);
